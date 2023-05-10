6a5e09c87f991f0ce83fdea4410ce1073b7a1782
NamDLD <120389990+DLDNam@users.noreply.github.com>
2023-05-11 00:51:11 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,9 @@
-import os
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
-stack_std = []
-i = 1
-
-#dọc file 
-file_data = open("Datastudent.txt", mode="r")
-while (1 != 2):
-    row = file_data.readline()
-    if row == "":
-        break
-    row_list = row.split(",")
-    row_list[4] = float(row_list[4])
-    stack_std.append([row_list[0], row_list[1], row_list[2], row_list[3], row_list[4]])
-
-def Delete(data,ID):
-    filter = [sv for sv in data if sv[0]!=ID]
-    return filter
-
-i = "21070878"
-stack_std = Delete(stack_std,i)
-for sv in stack_std:
-    print (sv)
+def CHX(st):
+    st = st.title()
+    st = st.strip()
+    kt = "  "
+    while kt in st:
+        st = st.replace("  "," ")
+    return st
+s = str(input())
+print(CHX(s))
